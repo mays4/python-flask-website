4d1fa000df1b9f5a5c7a167272143b087091e1be
mays4 <maysalani84@yahoo.com>
2022-02-11 14:29:56 -0500

    sign up form

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,38 @@
+from crypt import methods
+import email
 from xmlrpc.client import boolean
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
+  # data = request.form
+  # print(data)
   return render_template("login.html", boolean=False)
 
 @auth.route('/logout')
 def logout():
   return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
-   return render_template("sign_up.html")
+  if request.method =='POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2= request.form.get('password2')
+
+    if len(email)< 4:
+      flash("Email must be greater than 4 characters.",category='error')
+    elif len(firstName) < 2:
+      flash("first name must be greater than 1 characters.",category='error')
+    elif password1 != password2:
+      flash("Passwords don\'t match.",category='error')
+    elif len(password1) < 7 :
+      flash("Email must be at least 7 characters.",category='error')
+    else:
+      #add user to database
+      flash('Account Created!' ,category="success")
+
+  return render_template("sign_up.html")
